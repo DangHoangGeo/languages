4a0bc52ed2d20351b5739f0fa70cae8c3b595ff5
danghoanggeo <dang.hoang.geo@gmail.com>
2022-07-02 18:48:20 +0900

    add update grammar

--- pages/grammar/index.tsx
+++ pages/grammar/index.tsx
@@ -1,59 +1,84 @@
-import type { NextPage } from 'next'
-import * as React from 'react'
-import Pagination from '@mui/material/Pagination'
-import Layout from '../../components/layout/Layout'
-import {Section} from '../../components/layout/Section'
-import {getAllGrammarByLevel} from '../../utils/graphcms'
-import {IGrammar} from '../../types/grammar'
-import {IUser} from '../../types/user'
-import { LEANERID } from '../../utils/constants'
-
+import type { NextPage } from "next"
+import * as React from "react"
+import Button from '@mui/material/Button'
+import Pagination from "@mui/material/Pagination"
+import Layout from "../../components/layout/Layout"
+import FaqsCard from "../../components/FaqsCard"
+import { Section } from "../../components/layout/Section"
+import { getAllGrammarByLevel } from "../../utils/graphcms"
+import { updateGrammarLearner } from '../../utils/graphqlUpdate'
+import { IGrammar } from "../../types/grammar"
+import { IUser } from "../../types/user"
+import { LEANERID } from "../../utils/constants"
 
 type Props = {
   grammars: IGrammar[]
   user: IUser
 }
 
-const Grammar: NextPage<Props> = ({grammars, user}) => {
-  const [slected, setSelected] = React.useState(1);
-  const [grammar, setGrammar]  = React.useState(grammars[0]);
-  const [isNext, setIsNext] = React.useState(false);
+
+const Grammar: NextPage<Props> = ({ grammars, user }) => {
+  const [slected, setSelected] = React.useState(0)
+  const [onProcess, setOnProcess] = React.useState(false)
+  const [total, setTotal] = React.useState(grammars.length)
+  const [grammar, setGrammar] = React.useState(grammars[0])
+
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setSelected(value);
-    setGrammar(grammars[value-1]);
+    setGrammar(grammars[value - 1])
+    setSelected(value - 1)
+  }
+
+  async function handleRemember () {
+    setOnProcess(true)
+    const data = await updateGrammarLearner(LEANERID, grammar.id, false)
+    const index = grammars.indexOf(grammars[slected]);
+    if (index > -1) { // only splice array when item is found
+        grammars.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    setTotal(grammars.length)
+    setGrammar(grammars[slected+1])
+    setOnProcess(false)
   };
+
+
   return (
     <Layout>
       <Section title="N2 grammar" description="N2 grammar">
-      <div className="gap-4 justify-center">
-           <div className="flex justify-center">
-            <div className="indicator max-w-lg">
-                <div className="card border shadow-md">
-                    <div className="card-body space-y-4">
-                        <h2 className="card-title">{grammar.japanese}</h2> 
-                        <p>{grammar.english}</p>
-                        <div className="mt-4 p-4 bg-slate-500 text-gray-200 rounded-lg dark:text-slate-200"><code>{grammar.use.markdown}</code></div>
-                        <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
-                            <input type="checkbox" className="peer" /> 
-                            <div className="collapse-title font-medium">
-                                Example
-                            </div>
-                            <div className="collapse-content space-y-2"> 
-                                {grammar.sentences.map((sen, idx)=>(
-                                    <div className="border p-2 border-base-300 bg-base-100 rounded-box" key={idx}>
-                                        <p >{sen.japanese}</p>
-                                        <p>{sen.english}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+        <div>
+          <div className="flex justify-center">
+            <div className="indicator min-h-fit max-w-lg">
+                <div className="indicator-item indicator-top">
+                    <Button onClick={handleRemember} className="btn">Got</Button>
+                </div> 
+              <div className="card border shadow-md">
+                <div className="card-body space-y-4">
+                  <div className="text-center">
+                    <h2 className="text-2xl text-gray-800 font-semibold">
+                      {grammar.japanese}
+                    </h2>
+                  </div>
+                  <p>{grammar.english}</p>
+                  <div className="mt-4 p-4 bg-slate-500 text-gray-200 rounded-lg dark:text-slate-200">
+                    <code>{grammar.use.markdown}</code>
+                  </div>
+                  <div className="mx-auto">
+                    {grammar.sentences.map((sen, idx) => (
+                      <FaqsCard key={idx} faqsList={sen} idx={idx} />
+                    ))}
+                  </div>
                 </div>
+                {onProcess ? <span className="absolute inset-x-0 bottom-0 h-1  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"/>:<></>}
+              </div>
             </div>
-            </div>
-            <div className="mt-8">
-            <Pagination count={grammars.length} variant="outlined" onChange={handleChange} color="secondary" />
-            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <Pagination
+              count={total}
+              variant="outlined"
+              onChange={handleChange}
+              color="secondary"
+            />
+          </div>
         </div>
       </Section>
     </Layout>
@@ -61,8 +86,8 @@ const Grammar: NextPage<Props> = ({grammars, user}) => {
 }
 
 export async function getServerSideProps() {
-  const data = await getAllGrammarByLevel(LEANERID, "N2", 50, false)
-  return { props:{grammars: data.grammars, user: data.learner }}
+  const data = await getAllGrammarByLevel(LEANERID, "N2", 10, false)
+  return { props: { grammars: data.grammars, user: data.learner } }
 }
 
 export default Grammar
---
+++ utils/graphqlUpdate.ts
@@ -0,0 +1,95 @@
+//import {fetchAPI} from './graphcms'
+import {ILevel} from '../types/vocabulary'
+import {GRAPHCMS_DEV_AUTH_TOKEN, GRAPHCMS_PROD_AUTH_TOKEN, NEXT_CMS_GCMS_PROJECT_ID, GRAPHCMS_PROJECT_API} from '../utils/constants'
+
+type variablesType = {
+    id?:string
+    stage?: string
+    slug?: string
+    date?: Date
+    level?: ILevel
+    locales?: [string]
+    first?: number
+    orderBy?: string
+    learner?: string
+}
+
+interface Props {
+    variables?: variablesType,
+    preview?: boolean
+}
+
+export async function fetchAPI(query: string, {variables, preview}: Props = {}){
+    const res = await fetch(GRAPHCMS_PROJECT_API||"", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            Authorization: `Bearer ${
+                preview
+                ? GRAPHCMS_DEV_AUTH_TOKEN
+                : GRAPHCMS_PROD_AUTH_TOKEN
+                }`,
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    })
+
+    const json = await res.json()
+    if(json.errors) {
+        console.log(process.env.NEXT_CMS_GCMS_PROJECT_ID)
+        console.log(preview?"dev":"prod")
+        console.log(preview
+            ? process.env.GRAPHCMS_DEV_AUTH_TOKEN
+            : process.env.GRAPHCMS_PROD_AUTH_TOKEN)
+        console.error(json.errors)
+        throw new Error('Failed to fetch API')
+    }
+    return json.data
+}
+/**
+ * Description:
+ * connect user to remmbered grammar
+ * Params
+ * @param learner 
+ * @param grammarID 
+ * @param preview 
+ * 
+ * @output grammar
+ */
+export async function updateGrammarLearner(learner: string, grammarID: string, preview: boolean) {
+    const data = await fetchAPI(
+       `
+        mutation updateGrammar($id: ID!, $learner: ID!){
+            updateGrammar(
+                where: {id: $id},
+                data: { 
+                    learners: {
+                        connect: {
+                            where: {
+                                id: $learner
+                            }
+                        }
+                    }
+                }
+            ) {
+                id
+                japanese
+            }
+            publishGrammar( where: { id: $id }, to: PUBLISHED) {
+                id
+                japanese
+            }
+        }
+        `,{
+            preview,
+            variables:{
+                learner,
+                id: grammarID
+            }
+        }
+    )
+    return data
+}
